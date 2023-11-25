5c2b48e8c00a11079da9608700153e12e84d75b0
Матвей Клёнов <y0na24@mail.ru>
2023-11-25 21:48:48 +0300

    added header

---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import { nunito } from '@/fonts/fonts'
+import Image from 'next/image'
+import { FC } from 'react'
+
+export const Header: FC = () => {
+  return (
+    <header className={`${nunito.className} text-black px-5 py-3 bg-white`}>
+      <div className="flex items-center justify-between mx-auto max-w-[1920px]">
+        <div className="flex items-center gap-4">
+          <Image width={50} height={50} alt="Логотип компании" src="/logo.png" />
+          <h1 className={`text-[36px] `}>FitFuel</h1>
+        </div>
+        <nav>
+          <ul className="flex items-center text-lg gap-6">
+            <li className="group cursor-pointer transition duration-200">
+              <span>О нас</span>
+              <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-black"></span>
+            </li>
+            <li className="group cursor-pointer transition duration-200">
+              <span>Контакты</span>
+              <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-black"></span>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
---
+++ src/fonts/fonts.ts
@@ -0,0 +1,4 @@
+import { Nunito, PT_Sans } from 'next/font/google'
+
+export const nunito = Nunito({ subsets: ['latin'], weight: '700' })
+export const pt_sans = PT_Sans({ subsets: ['latin'], weight: '400' })
